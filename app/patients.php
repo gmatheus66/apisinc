39ec72025f8ea2b891ea86f9a3bfb6a09ef794d6
pphenriquesm <pphenriue543@gmail.com>
2020-01-21 19:42:32 -0300

    GH-2 Patients

---
+++ app/patients.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Patients extends Model
+{
+	protected $fillable = [
+		'name',
+		'age',
+		'sex',
+		'phone',
+		'Email',
+		'occupation',
+		'address',
+		'city',
+		'country',
+		'state_province',
+
+];
+	protected $guarded = [
+		'id';
+		'created_at',
+		'update_at';
+		'token_band';
+		'token_login';
+	]
+}
