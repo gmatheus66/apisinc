c7106024c5a16d877e97aee99368c68cc94d78ac
gmatheus66 <gmatheus66@hotmail.com>
2020-01-21 18:41:57 -0300

    GH-1 create migrations relatives

---
+++ database/migrations/2020_01_21_211516_create_relatives_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRelativesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('relatives', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('password');
+            $table->date('birthday');
+            $table->enum('sexo',['Male','Female','Another']);
+            $table->integer('telephone');
+            $table->string('email');
+            $table->string('ocupation');
+            $table->enum('kinship_degree',[
+                'Grandparents', 'Mother', 'Father','Grandchild', 'Son','Sister','Broher', 'Wife','Husband','Girlfriend','Boyfriend','Other']);
+            $table->string('address');
+            $table->string('city');
+            $table->string('contry');
+            $table->string('state_province');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('relatives');
+    }
+}
