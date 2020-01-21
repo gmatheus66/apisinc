90a76fdb33565642024931a67ea775839133b09a
gmatheus66 <gmatheus66@hotmail.com>
2020-01-21 19:52:25 -0300

    GH-1 create migration doctors institutions

---
+++ database/migrations/2020_01_21_224833_create_doctors_institutions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorsInstitutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors_institutions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('institution_id');
+            $table->foreign('institution_id')->references('id')->on('institutions');
+            $table->unsignedBigInteger('doctor_id');
+            $table->foreign('doctor_id')->references('id')->on('doctors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors_institutions');
+    }
+}
