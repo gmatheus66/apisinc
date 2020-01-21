3b7d2eea21da2b8054749cbbb163697b2d773c00
gmatheus66 <gmatheus66@hotmail.com>
2020-01-21 19:51:39 -0300

    GH-1 create migration institutions

---
+++ database/migrations/2020_01_21_223649_create_institutions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInstitutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('institutions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('CNPJ');
+            $table->string('address');
+            $table->string('city');
+            $table->string('contry');
+            $table->string('state_province');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('institutions');
+    }
+}
