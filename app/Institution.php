7a542c754f6335fb9fc0a6660f07b756534e8938
pphenriquesm <pedrohsmarinho@outlook.com>
2020-01-22 14:37:50 -0300

    GH-2 local

---
+++ app/Institution.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class institution extends Model
+{
+   protected $fillable [
+   	'name',
+   	'eni',
+   	'address',
+   	'city',	
+   	'contry',
+   	'state_province'
+   ]
+   protected $guarded [
+   	'eni'
+   ]
+}
