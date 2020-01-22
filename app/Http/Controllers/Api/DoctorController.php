f889daaaf4500143621d6e6673bf448b07dd4847
pphenriquesm <pedrohsmarinho@outlook.com>
2020-01-22 17:29:36 -0300

    GH-4 Doctor

---
+++ app/Http/Controllers/Api/DoctorController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Doctor;
+use Illuminate\Support\Facades\Validator;
+
+class DoctorController extends Controller
+{
+    private $doc;
+
+	public function __construct(Doctor $d){
+		$this->doc = $d;
+	}
+	public function index(){
+		if(sizeof($this->doc->all())<= 0){
+			return response()->json(['dados'=>'No doctors are registed']);
+		}
+		else{
+			return response()->json($this->doc->all(),200);
+		}
+	}
+}	public function store(Request $request){
+		$Validator = Validator::make($request->all(),[
+			'name' => 'required|string|min:2|max:50',
+			'crm' =>'required|numeric|max:8|unique:doctors,crm',
+			'specialization'=> 'required|string|min:5|max:50',
+			'institution_id'=>'required|string|min:5|max:50'
+		]);
+
+        if(sizeof($validator->errors()) > 0){
+            return response()->json($validator->errors());
+        }
+        try{
+
+        	$this->doc->create([
+        		'name'=> $request->get('name'),
+        		'crm'=> $request->get('crm'),
+        		'specialization'=> $request->get('specialization'),
+        		'institution_id'=> $request->get('institution_id')
+
+        	]);
+		return response()->json(['data' => ['msg' => 'Doctor registed successfully'] ],200);
+
+        }catch(Exception $e){
+		return response()->json($e);
+        }
+}
