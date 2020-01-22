555b5b93501e683d973a71aad6832deceab6277a
pphenriquesm <pphenriue543@gmail.com>
2020-01-22 14:02:43 -0300

    GH-2 Relatives

---
+++ app/relatives.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class relatives extends Model
+{
+   protected $fillable = [
+   	'name',
+   	'age',
+   	'birthday',
+   	'sex',
+   	'email',
+   	'password',
+   	'ocupation',
+   	'address',
+   	'city',
+   	'coutry',
+   	'state_province'
+
+   ]
+
+   protected $guarded = [
+   	'password'
+   ]
+}
