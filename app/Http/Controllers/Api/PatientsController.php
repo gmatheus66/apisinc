b2fb3a2ff20992d9caad0deae58ae574bfe3719b
gmatheus66 <gmatheus66@hotmail.com>
2020-01-22 15:46:29 -0300

    GH-4 modify and correction in patient controller

--- app/Http/Controllers/Api/PatientsController.php
+++ app/Http/Controllers/Api/PatientsController.php
@@ -1,22 +1,23 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Patient;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
 
 class PatientsController extends Controller
 {
-	private $paciente;
+	private $patient;
 
-	public function __construct(Patient $c){
-		$this->patient = $c;
+	public function __construct(Patient $p){
+		$this->patient = $p;
 	}
 
     public function index(){
     
-    if (sizeoff($this->patient-all()) <= 0) {
+    if (sizeof($this->patient->all()) <= 0) {
     	return response()->json(['data'=>['msg'=> 'Patient does not exist']]);
     }
     else{
