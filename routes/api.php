d8ade80235cb2594b4560b84b31fdcf85c55cbe6
gmatheus66 <gmatheus66@hotmail.com>
2020-01-22 15:44:59 -0300

    GH-3 Create route teste to patient

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::namespace('Api')->name('api.')->group(function(){
+
+    Route::prefix('patient')->group(function(){
+        Route::get('/', 'PatientsController@index')->name('index');
+    });
+});
