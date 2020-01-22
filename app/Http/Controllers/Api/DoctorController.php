cb6f726314266b64a865adf22118c32c1cdeac2b
gmatheus66 <gmatheus66@hotmail.com>
2020-01-22 17:37:24 -0300

    GH-4 Correction doctor controller

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Doctor;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
 
 class DoctorController extends Controller
 {
@@ -21,29 +22,31 @@ class DoctorController extends Controller
 			return response()->json($this->doc->all(),200);
 		}
 	}
-}	public function store(Request $request){
+	public function store(Request $request){
 		$Validator = Validator::make($request->all(),[
 			'name' => 'required|string|min:2|max:50',
 			'crm' =>'required|numeric|max:8|unique:doctors,crm',
 			'specialization'=> 'required|string|min:5|max:50',
 			'institution_id'=>'required|string|min:5|max:50'
-		]);
+			]);
+			
+			if(sizeof($validator->errors()) > 0){
+				return response()->json($validator->errors());
+			}
+			try{
+				
+				$this->doc->create([
+					'name'=> $request->get('name'),
+					'crm'=> $request->get('crm'),
+					'specialization'=> $request->get('specialization'),
+					'institution_id'=> $request->get('institution_id')
+					
+					]);
+					return response()->json(['data' => ['msg' => 'Doctor registed successfully'] ],200);
+					
+				}catch(Exception $e){
+					return response()->json($e);
+				}
+			}
+}	
 
-        if(sizeof($validator->errors()) > 0){
-            return response()->json($validator->errors());
-        }
-        try{
-
-        	$this->doc->create([
-        		'name'=> $request->get('name'),
-        		'crm'=> $request->get('crm'),
-        		'specialization'=> $request->get('specialization'),
-        		'institution_id'=> $request->get('institution_id')
-
-        	]);
-		return response()->json(['data' => ['msg' => 'Doctor registed successfully'] ],200);
-
-        }catch(Exception $e){
-		return response()->json($e);
-        }
-}
