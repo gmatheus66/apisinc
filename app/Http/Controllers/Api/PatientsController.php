7edcfee008178174947646775bdf0a90593c57c1
gmatheus66 <gmatheus66@hotmail.com>
2020-01-22 17:41:01 -0300

    GH-4 modify parametrs of patients

--- app/Http/Controllers/Api/PatientsController.php
+++ app/Http/Controllers/Api/PatientsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Patient;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class PatientsController extends Controller
 {
@@ -30,16 +31,17 @@ class PatientsController extends Controller
 
 		$validator = Validator::make($request->all(),[
 			'name' => 'required|string|max:50|min:3',
-			'age' => 'required|numeric|min:1|max:3',
+			'birthday' => 'required|date',
 			'sex' => 'required|in:Male,Female,Another',
-			'telephone' => 'required|numeric|min:8|max:9',
+			'telephone' => 'required|numeric',
 			'email' => 'required|max:50|min:10',
-			'ocupation' => 'required|string|max:50|min:5',
+			'occupation' => 'required|string|max:50|min:5',
 			'address' => 'required|string|max:60|min:5',
 			'city' => 'required|string|max:25|min:2',
-			'country' => 'required|string|min:4|max:20',
+			'country' => 'required|string',
 			'state_province' => 'required|string|min:2|max:20',
-			'zip' => 'required|numeric|min:5|max:9',
+			'zip' => 'required|numeric',
+			'password' => 'required'
 		]);
 
 		if(sizeof($validator->errors()) > 0 ){
@@ -49,16 +51,17 @@ class PatientsController extends Controller
 		try{
 			$this->patient->create([
 				'name' => $request->get('name'),
-				'age' => $request->get('age'),
+				'birthday' => $request->get('birthday'),
 				'sex' => $request->get('sex'),
 				'telephone' => $request->get('telephone'),
 				'email' => $request->get('email'),
-				'ocupation' => $request->get('ocupation'),
+				'occupation' => $request->get('occupation'),
 				'address' => $request->get('address'),
 				'city' => $request->get('city'),
 				'country' => $request->get('country'),
 				'state_province' => $request->get('state_province'),
 				'zip' => $request->get('zip'),
+				'password' => Hash::make($request->get('password'))
 			]);
 			return response()->json(['data' => ['msg' => 'Patient successfully registered'] ],200);
 		}
@@ -67,7 +70,47 @@ class PatientsController extends Controller
 		}
 	}
 
-	public function login(){
+	public function login(Request $request){
+
+		$validator = Validator::make($request->all(),[
+				'email' => 'required|string|email',
+				'password' => 'required|string',
+				'remember_me' => 'boolean'
+		]);
 		
+		if(sizeof($validator->errors()) > 0 ){
+			return response()->json($validator->errors());
+		}
+
+		try{
+
+			$credentials = request(['email', 'password']);
+
+			if(!Auth::attempt($credentials)){
+           	 return response()->json(['message' => 'Unauthorized' ]);
+			}
+			$user = $request->user();
+			$tokenResult = $user->createToken('Personal Access Token');
+			$token = $tokenResult->token;
+			
+			if ($request->remember_me){
+
+				$token->expires_at = Carbon::now()->addWeeks(1);
+			}
+
+			$token->save();
+			return response()->json([
+				'access_token' => $tokenResult->accessToken,
+				'token_type' => 'Bearer',
+				'expires_at' => Carbon::parse(
+					$tokenResult->token->expires_at
+				)->toDateTimeString()
+			]);
+
+		}
+		catch(Exception $e){
+			return response()->json($e);
+		}
+
 	}
 }
