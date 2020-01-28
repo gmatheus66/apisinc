61afb6afb96319de6c6b1a9fb2b6c6529cc56198
gmatheus66 <gmatheus66@hotmail.com>
2020-01-28 13:56:03 -0300

    GH-3 Add routes to register and login

--- routes/api.php
+++ routes/api.php
@@ -25,13 +25,21 @@ Route::namespace('Api')->name('api.')->group(function(){
     //Route::get('/logout', 'AuthController@logout')->middleware('auth:api');
 
     Route::prefix('patient')->group(function(){
-
         Route::get('/', 'PatientsController@index')->name('index')->middleware('auth:api');
+        Route::get('/{id}', 'PatientsController@show')->name('show_patient')->middleware('auth:api');
         Route::post('/register', 'PatientsController@register')->name('register_patient');
         Route::post('/login', 'PatientsController@login')->name('login_patient');
     });
     Route::prefix('doctor')->group(function(){
     	Route::get('/','DoctorController@index')->name('index');
-    	Route::post('/store','DoctorController@store')->name('store');
+    	Route::post('/register','DoctorController@register')->name('register_doctor');
+        Route::post('/login', 'DoctorController@login')->name('login_doctor');
+    });
+    Route::prefix('institutions')->group(function(){
+        Route::post('/register', 'InstitutionsController@register')->name('register_institution');
+    });
+    Route::prefix('relatives')->group(function(){
+        Route::post('/register', 'RelativesController@register')->name('register_relatives');
+        Route::post('/login', 'RelativesController@login')->name('login_register');
     });
 });
