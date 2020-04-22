ff1ec03b1fbe4bae815325933c9cc59d88d2aeaf
gmatheus66 <gmatheus66@hotmail.com>
2020-04-22 17:42:33 -0300

    GH-3 Adicionando rota para check user para Familiar Doutor

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ Route::group(['prefix' => 'doctor','middleware' => ['assign.guard:doctors','jwt.
 {
 	Route::post('/handbook/register','HandbookController@register')->name('register_handbook');	
 	Route::get('/patient', 'PatientController@get_all_patient_doctor')->name('patient_relative');
+	Route::get('/check', 'DoctorController@check_user')->name('check_doctor');
 });
 Route::group(['prefix' => 'relative','middleware' => ['assign.guard:relatives','jwt.auth']],function ()
 {
@@ -41,6 +42,7 @@ Route::group(['prefix' => 'relative','middleware' => ['assign.guard:relatives','
 	Route::get('/patients', 'PatientController@get_all_patient_relatives')->name('patient_relative');
 	Route::get('/patient', 'RelativePatientController@get_my_patient_relatives')->name('patient_relative');
 	Route::post('/register/patient', 'RelativePatientController@register')->name('register_patient_relative');
+	Route::get('/check', 'RelativeController@check_user')->name('check_relative');
 });
 Route::group(['prefix' => 'patient','middleware' => ['assign.guard:patient','jwt.auth']],function ()
 {
