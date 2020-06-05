35fd1da4a6421987c7968bdfad9635e398f8336c
gmatheus66 <gmatheus66@hotmail.com>
2020-06-05 14:46:02 -0300

    GH-15 Add Documentação do login logout do patient

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -15,6 +15,25 @@ use Illuminate\Support\Facades\DB;
 
 class PatientController extends Controller
 {
+    /**
+    * @OA\Get(
+    *     path="/relative/patients",
+    *     description="Returns a list of patients for the relative",
+    *     tags={"Relative"},
+    *     @OA\Response(
+    *         response=200,
+    *         description="OK",
+    *     ),
+    *     @OA\Response(
+    *         response=422,
+    *         description="Missing Data"
+    *     ),
+    *     @OA\Response(
+    *          response=401,
+    *          description="Unauthenticated",
+    *     ),
+    * )
+    */
 	public function get_all_patient_relatives(){
 		$patients = Patient::all();
 		return response()->json($patients);
@@ -61,7 +80,7 @@ class PatientController extends Controller
 				'zip' => $request->get('zip'),
 				'password' => Hash::make($request->get('password')),
 			]);
-            
+
             $token = auth('patient')->login($pat);
 
 			return $this->respondWithToken($token);
@@ -80,7 +99,26 @@ class PatientController extends Controller
             'user' => auth('patient')->user()
         ]);
     }
-    
+
+    /**
+    * @OA\Post(
+    *     path="/patient/logout",
+    *     description="Logout Patient",
+    *     tags={"Patient"},
+    *     @OA\Response(
+    *         response=200,
+    *         description="OK",
+    *     ),
+    *     @OA\Response(
+    *         response=422,
+    *         description="Missing Data"
+    *     ),
+    *     @OA\Response(
+    *          response=401,
+    *          description="Unauthenticated",
+    *     ),
+    * )
+    */
     public function logout()
     {
 		config([
@@ -91,14 +129,45 @@ class PatientController extends Controller
         return response()->json(['message' => 'Successfully logged out']);
     }
 
+    /**
+    * @OA\Post(
+    *     path="/patient/login",
+    *     description="Login Patient",
+    *     tags={"Patient"},
+    *     @OA\Parameter(
+    *         name="email",
+    *         in="query",
+    *         description="Email",
+    *         required=true,
+    *     ),
+    *     @OA\Parameter(
+    *         name="password",
+    *         in="query",
+    *         description="Password",
+    *         required=true,
+    *     ),
+    *     @OA\Response(
+    *         response=200,
+    *         description="OK",
+    *     ),
+    *     @OA\Response(
+    *         response=422,
+    *         description="Missing Data"
+    *     ),
+    *     @OA\Response(
+    *          response=401,
+    *          description="Unauthenticated",
+    *     ),
+    * )
+    */
     public function login(Request $request){
 
 		$loginData = $request->validate([
             'email' => 'email|required',
             'password' => 'required'
 		]);
-		
-		
+
+
 		$credentials = request(['email', 'password']);
 
         if (! $token = auth('patient')->attempt($credentials)) {
@@ -107,11 +176,11 @@ class PatientController extends Controller
 
 		return $this->respondWithToken($token);
     }
-    
+
     public function detail_auth_user(){
 		return auth('patient')->user();
 	}
-	
+
 	public function check_user(){
 
 		try{
@@ -119,29 +188,29 @@ class PatientController extends Controller
 		}
 		catch (UnauthorizedHttpException $e) {
 			return response()->json(false);
-		} 
+		}
 		catch (TokenBlacklistedException $e) {
 			return response()->json(false);
-		} 
+		}
 		catch (TokenExpiredException $e) {
 			return response()->json(false);
-		} 
+		}
 		catch (TokenInvalidException $e) {
 			return response()->json(false);
-		} 
+		}
 		catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
 			return response()->json(false);
-		} 
+		}
 		catch (Tymon\JWTAuth\Exceptions\TokenBlacklistedException $e) {
 			return response()->json(false);
-		} 
+		}
 		catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
 			return response()->json(false);
 		}
 		catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
 			return response()->json(false);
 		}
-		
+
 	}
 
 }
